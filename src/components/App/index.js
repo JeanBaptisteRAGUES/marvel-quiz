b803b167777e8268b6fc402d5c41a217b0783aed
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2021-07-25 00:16:16 +0200

    First commit : création des 3 premiers composants de base (Header, Landing et Footer) avec leur css respectif

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import Header from '../Header'
+import Landing from '../Landing'
+import Footer from '../Footer'
+import '../../App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Landing />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;

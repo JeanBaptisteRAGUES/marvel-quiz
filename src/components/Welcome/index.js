bf8bb1807a2f4ed32f4c0ad9e173332bc26bedd1
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2021-08-02 01:29:17 +0200

    Gestion de la session utilisateur via firebase

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Fragment, useContext, useEffect, useState } from 'react'
 import Logout from '../Logout';
 import Quiz from '../Quiz';
+import { FirebaseContext } from '../Firebase';
 
-const Welcome = () => {
-    return (
+const Welcome = (props) => {
+
+    const firebase = useContext(FirebaseContext);
+    const [userSession, setUserSession] = useState(null);
+
+    useEffect(() => {
+
+        let listener = firebase.auth.onAuthStateChanged(user => {
+            user ? setUserSession(user) : props.history.push('/');
+        })
+
+        return () => {
+            //Arrête le listener
+            listener();
+        };
+    }, [])
+
+    return userSession === null ? (
+        <Fragment>
+            <div className="loader"></div>
+            <p>Loading..</p>
+        </Fragment>
+    ) : (
         <div className="quiz-bg">
             <div className="container">
                 <Logout />

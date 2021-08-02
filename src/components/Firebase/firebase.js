b8c0c39e978240f3791e61a022e6d72cce3510cf
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2021-08-02 12:37:22 +0200

    Ajout d'un nouvel utilisateur dans Firestore Database lors de l'inscription de celui-ci

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
     apiKey: "AIzaSyC1il-u8OODTPY6sUjljV9jTqA3KlZA860",
@@ -14,6 +15,7 @@ class Firebase{
     constructor() {
         app.initializeApp(config);
         this.auth = app.auth();
+        this.db = app.firestore();
     }
 
     // Inscription
@@ -33,6 +35,9 @@ class Firebase{
 
     // Récupérer le mot de passe
     passwordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+    // Créer un nouveau document dans users avec pour id l'uid généré automatiquement lors de l'authentification via Firebase
+    user = (uid) => this.db.doc(`users/${uid}`);
 }
 
 export default Firebase
